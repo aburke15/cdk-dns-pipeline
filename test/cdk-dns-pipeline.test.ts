1b5a4519147faad5ca0d7419ddd40b09a6a156cd
Andre Burke <aburke.dev@gmail.com>
2022-07-30 16:45:36 -0600

    Initial commit

---
+++ test/cdk-dns-pipeline.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as CdkDnsPipeline from '../lib/cdk-dns-pipeline-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/cdk-dns-pipeline-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new CdkDnsPipeline.CdkDnsPipelineStack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
